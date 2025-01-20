53fdfe8a52d1a9af02e3a0cff2593cbbe6abc133
Alef-Daniel-Mello <alefdanieldemellocoutinho@gmail.com>
2025-01-20 12:56:24 +0000

    WIP Mestre

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,57 +7,110 @@
 //Teste larissa
 
 int main() {
-    int CityCode = 0;
-    char CityName[50] = "None";
-    int CityPopulation = 0;
-    float CityArea = 0;
-    float CityPIB = 0;
-    int CityPlaces = 0;
+    int City1Code, City2Code = 0;
+    char City1Name[50], City2Name[50] = "None";
+    int City1Population, City2Population = 0;
+    float City1Area, City2Area = 0;
+    float City1PIB, City2PIB = 0;
+    int City1Places, City2Places = 0;
     
-    float Density = 0;
-    float PIBpC = 0;
+    float Density1, Density2 = 0;
+    float PIBpC1, PIBpC2 = 0;
 
      // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
     
     printf("Insira o código da cidade :\n");
-    scanf("%d",&CityCode);
-    printf("Código da cidade definido como : %d\n", CityCode);
+    scanf("%d",&City1Code);
+    printf("Código da cidade definido como : %d\n", City1Code);
 
     printf("Agora, insira o nome da cidade :\n");
-    scanf("%s",&CityName);
-    printf("Nome da cidade definido como : %s\n", CityName);
+    scanf("%s",&City1Name);
+    printf("Nome da cidade definido como : %s\n", City1Name);
 
     printf("Agora insira a população da cidade :\n");
-    scanf("%d",&CityPopulation);
-    printf("População da cidade definida como : %d\n", CityPopulation);
+    scanf("%d",&City1Population);
+    printf("População da cidade definida como : %d\n", City1Population);
 
     printf("Agora insira o PIB :\n");
-    scanf("%f",&CityPIB);
-    printf("PIB definido como : %f\n", CityPIB);
+    scanf("%f",&City1PIB);
+    printf("PIB definido como : %f\n", City1PIB);
 
     printf("Insira também a área da cidade em metros quadrados :\n");
-    scanf("%f",&CityArea);
-    printf("Área da cidade definida como : %f metros quadrados\n", CityArea);
+    scanf("%f",&City1Area);
+    printf("Área da cidade definida como : %f metros quadrados\n", City1Area);
 
     printf("Por último, insira a quantidade de pontos turísticos da cidade :\n");
-    scanf("%d",&CityPlaces);
-    printf("Quantidade de pontos turísticos definida como : %d\n", CityPlaces);
+    scanf("%d",&City1Places);
+    printf("Quantidade de pontos turísticos definida como : %d\n", City1Places);
     
-    PIBpC = CityPIB / CityPopulation;
-    Density = CityPopulation / CityArea;
+    PIBpC1 = City1PIB / City1Population;
+    Density1 = City1Population / City1Area;
 
     printf("Valores definidos, mostrando resultados :\n");
+    printf(" \n");
 
-    printf("Nome : %s\n", CityName);
-    printf("Código : %d\n", CityCode);
-    printf("População : %d\n", CityPopulation);
-    printf("PIB : %f\n", CityPIB);
-    printf("PIB per Capita : %f\n", PIBpC);
-    printf("Área : %f metros quadrados\n", CityArea);
-    printf("Densidade populacional : %f\n", Density);
-    printf("Pontos turísticos : %d\n", CityPlaces);
+    printf("Nome : %s\n", City1Name);
+    printf("Código : %d\n", City1Code);
+    printf("População : %d\n", City1Population);
+    printf("PIB : %f\n", City1PIB);
+    printf("PIB per Capita : %f\n", PIBpC1);
+    printf("Área : %f metros quadrados\n", City1Area);
+    printf("Densidade populacional : %f\n", Density1);
+    printf("Pontos turísticos : %d\n", City1Places);
 
+    printf(" \n");
+    printf("Agora, iremos configurar a segunda cidade\n");
+
+// Segunda cidade
+
+    printf("Insira o código da cidade :\n");
+    scanf("%d",&City2Code);
+    printf("Código da cidade definido como : %d\n", City2Code);
+
+    printf("Agora, insira o nome da cidade :\n");
+    scanf("%s",&City2Name);
+    printf("Nome da cidade definido como : %s\n", City2Name);
+
+    printf("Agora insira a população da cidade :\n");
+    scanf("%d",&City2Population);
+    printf("População da cidade definida como : %d\n", City2Population);
+
+    printf("Agora insira o PIB :\n");
+    scanf("%f",&City2PIB);
+    printf("PIB definido como : %f\n", City2PIB);
+
+    printf("Insira também a área da cidade em metros quadrados :\n");
+    scanf("%f",&City2Area);
+    printf("Área da cidade definida como : %f metros quadrados\n", City2Area);
+
+    printf("Por último, insira a quantidade de pontos turísticos da cidade :\n");
+    scanf("%d",&City2Places);
+    printf("Quantidade de pontos turísticos definida como : %d\n", City2Places);
+    
+    PIBpC2 = City2PIB / City2Population;
+    Density2 = City2Population / City2Area;
+
+    printf("Valores definidos, mostrando resultados :\n");
+    printf(" \n");
+
+    printf("Nome : %s\n", City2Name);
+    printf("Código : %d\n", City2Code);
+    printf("População : %d\n", City2Population);
+    printf("PIB : %f\n", City2PIB);
+    printf("PIB per Capita : %f\n", PIBpC2);
+    printf("Área : %f metros quadrados\n", City2Area);
+    printf("Densidade populacional : %f\n", Density2);
+    printf("Pontos turísticos : %d\n", City2Places);
+
+    printf(" \n");
+    printf("Cartas cadastradas, iniciando embate!\n");
+    printf(" \n");
+
+    int PopulationFight = City1Population > City2Population;
+
+    printf("Comparando população...\n");
+    
     // Cadastro das Cartas:
     // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
     // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
